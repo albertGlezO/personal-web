c212102e3a0748adaa6ae1d03d0e93ed358ea66c
Albert Gonzalez <albertgonz4954@gmail.com>
2021-10-23 00:27:48 -0500

    [FEAT] librearia bulma cargada con node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bulma/css/bulma.min.css';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
