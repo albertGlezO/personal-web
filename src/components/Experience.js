5f4fa17402971e5c1b4e794708c3917bd09f4f03
Albert Gonzalez <albertgonz4954@gmail.com>
2021-10-29 11:00:41 -0500

    [ADD] componente faltante

---
+++ src/components/Experience.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+function Experience() {
+    return (
+        <div className="mb-6">
+            <div className="container">
+                <div className="columns" id="experience">
+                    <div className="column content">
+                        <center><h1>Experience</h1></center>
+                    </div>
+                </div>
+                <hr/>
+                <div className="columns">
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                    <div className="column">
+                        <figure className="image is-128x128">
+                            <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder"/>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <nav className="level is-mobile">
+                <div className="level-item has-text-centered">
+                    <div>
+                    <p className="heading is-size-4">Años de experiencia</p>
+                    <p className="title">+6</p>
+                    </div>
+                </div>
+                <div className="level-item has-text-centered">
+                    <div>
+                    <p className="heading is-size-4">Participacion en proyectos</p>
+                    <p className="title">30</p>
+                    </div>
+                </div>
+                <div className="level-item has-text-centered">
+                    <div>
+                    <p className="heading is-size-4">Proyectos Dirigidos</p>
+                    <p className="title">10</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Experience;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import 'bulma/css/bulma.min.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Biography from './components/Biography';
+import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Blog from './components/Blog';
 import Footer from './components/Footer';
 
 ReactDOM.render(
-  <div><Navbar/><Home/><Biography/><Skills/><Blog/><Footer/></div>,
+  <div><Navbar/><Home/><Biography/><Experience/><Skills/><Blog/><Footer/></div>,
   document.getElementById('root')
 );
